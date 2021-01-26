f5a17489b73fef14bf40ce9017dcc2fc799f8f85
Amankaium <kaium@list.ru>
2021-01-26 20:34:45 +0600

    added user and good in order model

--- core/views.py
+++ core/views.py
@@ -8,15 +8,6 @@ class GoodViewSet(viewsets.ModelViewSet):
     queryset = Good.objects.filter(qty__gt=0)
     serializer_class = GoodSerializer
 
-    # def get_queryset(self):
-    #     return Good.objects.all()
-
-
-# def test(request):
-#     if request.method = "GET":
-#         #...
-#     elif request.method == "POST":
-#         # ...
 
 class OrderCreate(CreateAPIView):
     serializer_class = OrderSerializer
